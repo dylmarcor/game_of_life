07e16fac6179e87cf3459bbed88024877cf9a611
dylmarcor <dylancorbett@icloud.com>
2022-02-14 16:46:59 -0800

    add census fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate rand;
+use std::{thread, time};
+
+fn census(_world: [[u8; 75]; 75]) -> u16 {
+    let mut count = 0;
+
+    for i in 0..74 {
+        for j in 0..74 {
+            if _world[i][j] == 1 {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
 fn main() {
     println!("Hello, world!");
 }
